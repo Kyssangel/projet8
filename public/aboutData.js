6c79f89a835ed929d91be3039ab9508f7cc8344f
Robert christian <kyssangel18@gmail.com>
2024-03-22 17:00:58 +0100

    rectification

--- public/aboutData.js
+++ public/aboutData.js
@@ -1,5 +1,5 @@
 const aboutData ={
-    slogan: "Chez vous, partout et ailleurs",
+   
     fiability:
       "Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes.",
     respect:
