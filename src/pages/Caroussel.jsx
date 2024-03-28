1540a2fc1f1950c56a2b67fe2b55c4e7188462b5
Robert christian <kyssangel18@gmail.com>
2024-03-28 15:47:51 +0100

    amelioration du caroussel

--- src/pages/Caroussel.jsx
+++ src/pages/Caroussel.jsx
@@ -1,5 +1,5 @@
 
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import logements from "../../public/logements.json";
 import chevronGauche from "../assets/images/ChevronGauche.png";
@@ -7,32 +7,20 @@ import chevronDroit from "../assets/images/ChevronDroit.png";
 
 export default function Carousel({ logementId }) {
     const [index, setIndex] = useState(0);
-    const [logement, setLogement] = useState(null);
 
     useEffect(() => {
-        setLogement(findLogementByID(logementId));
-        setIndex(0); 
+        const foundLogement = logements.find(logement => logement.id === logementId);
+        if (foundLogement) {
+            const totalPictures = foundLogement.pictures.length - 1;
+            setIndex(prevIndex => prevIndex < 0 ? totalPictures : prevIndex > totalPictures ? 0 : prevIndex);
+        }
     }, [logementId]);
 
-    useEffect(() => {
-        if (logement) {
-            const totalPictures = logement.pictures.length - 1;
-            if (index < 0) setIndex(totalPictures);
-            if (index > totalPictures) setIndex(0);
-        }
-    }, [index, logement, logementId]); 
+    const nextSlide = () => setIndex(prevIndex => (prevIndex + 1) % logements.find(logement => logement.id === logementId).pictures.length);
+    const prevSlide = () => setIndex(prevIndex => (prevIndex - 1 + logements.find(logement => logement.id === logementId).pictures.length) % logements.find(logement => logement.id === logementId).pictures.length);
 
-    if (!logement) {
-        return null; 
-    }
-
-    const nextSlide = () => {
-        setIndex((prevIndex) => (prevIndex === logement.pictures.length - 1 ? 0 : prevIndex + 1));
-    };
-
-    const prevSlide = () => {
-        setIndex((prevIndex) => (prevIndex === 0 ? logement.pictures.length - 1 : prevIndex - 1));
-    };
+    const logement = logements.find(logement => logement.id === logementId);
+    if (!logement) return null;
 
     return (
         <>
@@ -41,18 +29,13 @@ export default function Carousel({ logementId }) {
                 className="classImage"
                 key={"car-" + index}
                 alt={"photo " + index}
-                
             />
             {logement.pictures.length > 1 && (
                 <>
                     <img src={chevronGauche} className='classChevronGauche' alt="flèche gauche pour changer de photo" onClick={prevSlide} />
                     <img src={chevronDroit} className='classChevronDroit' alt="flèche droite pour changer de photo" onClick={nextSlide} />
+                    <p className='compteurImages'>{index + 1}/{logement.pictures.length}</p>
                 </>
-            )}
-            {logement.pictures.length > 1 && (
-                <p className='compteurImages'>
-                    {index + 1}/{logement.pictures.length}
-                </p>
             )}
         </>
     );
@@ -62,6 +45,4 @@ Carousel.propTypes = {
     logementId: PropTypes.string.isRequired
 };
 
-const findLogementByID = (id) => {
-    return logements.find((logement) => logement.id === id);
-};
+
